420d779a59138dfa69fb5821d2de3c632f601495
Israel Santana <113399087+SantanaIsrael@users.noreply.github.com>
2022-12-12 17:50:39 -0300

    update

--- main.java
+++ main.java
@@ -1,10 +1,10 @@
 public class main {
-    public static void main(String[] args){
-        Data hoje = new Data(11,12,2022);
+    public static void main(String[] args) {
+        Data hoje = new Data(11, 12, 2022);
         Aluno aluno = new Aluno("israel", "algumacoisa.com", "rua dos bobos", 3, 88, 074, hoje);
-        Livro livro = new Livro("Coisas de nerd", "flamingos",3233, 300, 1);
+        Livro livro = new Livro("Coisas de nerd", "flamingos", 3233, 300, 1);
 
-        //livro.setAluno(null);
+        // livro.setAluno(null);
         System.out.println("Antes do emprestimo: " + livro.getAluno());
         livro.setAluno(aluno);
         System.out.println("Depois do emprestimo: " + livro.getAluno());
